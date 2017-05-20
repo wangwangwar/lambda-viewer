6b3b9e10df31d87acb90949e0cc4ed82fb7fc954
wangwangwar <wangwangwar@gmail.com>
2017-05-20 11:57:06 +0800

    Ex06

--- program/prog-06.hs
+++ program/prog-06.hs
@@ -1,5 +1,5 @@
 -- Program that goes with "Learn You a Lambda, a Haskell Tutorial", Chapter 6.
--- 
+--
 -- We use the QuickCheck library to help check program correctness.
 -- E.g. "quickCheck propTerm" will show that all tests pass for the
 -- lambda parser given at the end of this file.
@@ -7,9 +7,10 @@
 -- You can use "quickCheck propSpaced" to test solutions to problem 1,
 -- or "quickCheck propPretty" to test solutions to problem 2.
 
-import Data.Char
-import Test.QuickCheck
-import Control.Applicative hiding (many, (<|>))
+import           Control.Applicative hiding (many, (*>), (<*), (<|>))
+import           Data.Char
+import           Prelude             hiding ((*>), (<*))
+import           Test.QuickCheck
 
 -- Datatype definition for lambda expression.
 data Term = Var Var | Lam Var Term | App Term Term deriving Eq
@@ -27,11 +28,11 @@ instance Show Term where
 -- Pretty-print that minimizes the number of parentheses.
 -- (solution to problem 2 in tutorial 3)
 pretty = snd . fold i g h
-  where 
+  where
     i (V v)       = (either id id, v)
     g (V v) (_,e) = (either pr pr, "λ" ++ v ++ "." ++ e)
     h (b,f) (d,e) = (either id pr, b (Left f) ++ " " ++ d (Right e))
-    pr s = "(" ++ s ++ ")" 
+    pr s = "(" ++ s ++ ")"
 
 -- Generic fold on Term, used by pretty.
 fold :: (Var -> a) -> (Var -> a -> a) -> (a -> a -> a) -> Term -> a
@@ -53,12 +54,14 @@ mapP :: (a -> b) -> ReadS a -> ReadS b
 mapP f g = map (\ (c, s) -> (f c, s)) . g
 
 (&&&) :: ReadS a -> ReadS b -> ReadS (a, b)
-f &&& g = \s -> [ ((x, y), s2) 
-                | (x, s1) <- f s, 
+f &&& g = \s -> [ ((x, y), s2)
+                | (x, s1) <- f s,
                   (y, s2) <- g s1 ]
 
 (|||) :: ReadS a -> ReadS b -> ReadS (Either a b)
-f ||| g = \s -> map left (f s) ++ map right (g s)
+f ||| g = \s -> case f s of
+                  [] -> map right (g s)
+                  xs -> map left xs
   where left  (x, s) = (Left  x, s)
         right (y, s) = (Right y, s)
 
@@ -67,20 +70,25 @@ f <|> g = mapP select (f ||| g)
   where select (Left  x) = x
         select (Right y) = y
 
+(<*) :: ReadS a -> ReadS b -> ReadS a
+f <* g = mapP (\(a, _) -> a) $ f &&& g
+
+(*>) :: ReadS a -> ReadS b -> ReadS b
+f *> g = mapP (\(_, b) -> b) $ f &&& g
+
 many, many1 :: ReadS a -> ReadS [a]
 many r  = many1 r <|> nil
 many1 r = mapP cons (r &&& many r)
   where cons (x, xs) = x : xs
 
-paren p = mapP f (sym '(' &&& p &&& sym ')')
-  where f ((_, x), _) = x
+paren p = sym '(' *> (space ||| nil) *> p <* (space ||| nil) <* sym ')'
 
 -- Read instance for Var and Term.
 instance Read Var where
-  readsPrec _ = variable 
+  readsPrec _ = variable
 
 instance Read Term where
-  readsPrec _ = term 
+  readsPrec _ = term
 
 -- Randomly generate Var and Term for QuickCheck.
 instance Arbitrary Var where
@@ -100,7 +108,7 @@ instance Arbitrary Term where
         where m = n `div` 2
 
 
--- Show, Read, Arbitrary instances for Pretty 
+-- Show, Read, Arbitrary instances for Pretty
 data Pretty = Pretty Term deriving Eq
 
 instance Show Pretty where
@@ -113,7 +121,7 @@ instance Arbitrary Pretty where
   arbitrary = fmap Pretty arbitrary
 
 -- Randomly insert spaces into a string.
-data Spaced a = Spaced a [Bool] 
+data Spaced a = Spaced a [Bool]
 
 instance Eq a => Eq (Spaced a) where
   Spaced x _ == Spaced y _ = x == y
@@ -128,7 +136,7 @@ instance Show a => Show (Spaced a) where
           notVar x (y:_) = not (isAlphaNum x && isDigit y)
 
 instance Read a => Read (Spaced a) where
-  readsPrec = mapP f . readsPrec 
+  readsPrec = mapP f . readsPrec
     where f x = Spaced x []
 
 instance Arbitrary a => Arbitrary (Spaced a) where
@@ -169,7 +177,7 @@ atom  = lam <|> var <|> paren term
 
 var = mapP Var variable
 
-lam = mapP f (lbd &&& variable &&& sym '.' &&& term)
+lam = mapP f (lbd &&& ((space ||| nil) *> variable <* (space ||| nil)) &&& sym '.' &&& ((space ||| nil) *> term))
    where f (((_, v), _), e) = Lam v e
 
 lbd = (sym '\\' <|> sym 'λ')
@@ -178,3 +186,28 @@ sym = char . (==)
 
 space = many1 (sym ' ')
 
+main = do
+  quickCheck propTerm
+  quickCheck propPretty
+  quickCheck propSpaced
+  quickCheck propSpacedPretty
+  print $ term "λx.x x"
+  print $ term "(λx.x) x"
+
+-- 一、我们定义了 term' 来处理空格，但这种方法不是特别全面，比如括号后面的空格，或者符号 . 前后的空格，
+-- 都会造成识别失败。能否有一个更好的处理空格的方式？需要怎样修改程序？
+--
+-- A: 去掉 `|||` 的回溯。添加空格的识别。
+--
+-- 二、尽管我们避免了识别应用形态λ项的歧义，但是在使用λ抽象时，依然会有产生歧义的情况。
+-- 比如 term "λx.x x" 会得到三个结果：
+--
+-- ```
+-- [((λx.(x x)),""),(((λx.x) x),""),((λx.x)," x")]
+-- ```
+--
+-- 前两个都完整地匹配了输入字串，这造成 read "λx.x x" :: Term 会得到错误
+-- Exception: Prelude.read: ambiguous parse。假设我们定义λ的作用域是延展到最右边，
+-- 那么应该如何修改语法，让 read "λx.x x" 正确读出唯一的结果 (λx.(x x))？
+--
+-- A: 目前的parser可以读出唯一正确的结果。
